5f7ee74e8f9afa25914022236a04745ab9e61909
arya <arya@Aryas-MacBook-Pro.local>
2025-05-04 19:57:38 -0700

    add my name

--- lib/views/pages/items_page.dart
+++ lib/views/pages/items_page.dart
@@ -21,57 +21,72 @@ class ItemsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(30),
-        crossAxisSpacing: 15,
-        mainAxisSpacing: 15,
-        children:
-            items.map((item) {
-              return GestureDetector(
-                onTap: () {
-                  if (item['destination'] != '') {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => item['destination'],
-                      ),
-                    );
-                  }
-                  else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Coming soon!'),
-                        duration: const Duration(seconds: 2),
-                      ),
-                    );
-                  }
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[900],
-                      borderRadius: BorderRadius.circular(35),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          item['desc']!,
-                          style: const TextStyle(fontSize: 30),
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(30),
+          crossAxisSpacing: 15,
+          mainAxisSpacing: 15,
+          children:
+              items.map((item) {
+                return GestureDetector(
+                  onTap: () {
+                    if (item['destination'] != '') {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => item['destination'],
                         ),
-                        const SizedBox(height: 20),
-                        Text(
-                          item['name']!,
-                          style: const TextStyle(color: Colors.white70),
+                      );
+                    }
+                    else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Coming soon!'),
+                          duration: const Duration(seconds: 2),
                         ),
-                      ],
+                      );
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[900],
+                        borderRadius: BorderRadius.circular(35),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            item['desc']!,
+                            style: const TextStyle(fontSize: 30),
+                          ),
+                          const SizedBox(height: 20),
+                          Text(
+                            item['name']!,
+                            style: const TextStyle(color: Colors.white70),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            }).toList(),
+                );
+              }).toList(),
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 50,
+        width: double.infinity,
+        child: Center(
+          child: Text('Made with ❤️ by Arya', 
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 12,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
       ),
     );
   }
